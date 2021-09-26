e96d609fb886812d0d7f5d6aa59af6e8d269fa0e
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-09-26 13:41:23 +0330

    class Company added

---
+++ src/Company.ts
@@ -0,0 +1,19 @@
+import { company, address } from 'faker';
+
+export class Company {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = company.companyName();
+    this.catchPhrase = company.catchPhrase();
+    this.location = {
+      lat: +address.latitude(),
+      lng: +address.longitude(),
+    };
+  }
+}
