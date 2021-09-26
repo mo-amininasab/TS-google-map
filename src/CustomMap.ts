2699303109b855f8fd3c3e9c3277a8b70153271b
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-09-26 17:32:46 +0330

    infoWindow added to markers

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,11 @@
 interface Mappable {
   location: { lat: number; lng: number };
+  markerContent(): string;
 }
 
 export class CustomMap {
   private googleMap: google.maps.Map;
-  
+
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId)!, {
       zoom: 2,
@@ -13,12 +14,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
--- src/User.ts
+++ src/User.ts
@@ -14,4 +14,8 @@ export class User {
       lng: +address.longitude(),
     };
   }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`
+  }
 }
